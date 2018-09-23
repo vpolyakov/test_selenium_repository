2f3a185d44ea53b5ef90d0b470bacd6f531a9828
Vladimir Polyakov <vlpolyakov@mail.ru>
2018-09-23 20:51:54 +0400

    Задание 1. Добавил проект в репозиторий и тест на python.

---
+++ test_example.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://www.google.com/")
+    wait = WebDriverWait(driver, 10)  # seconds
+    driver.find_element_by_name("q").send_keys("webdriver")
+    driver.find_element_by_name("q").send_keys(Keys.ENTER)
+    wait.until(EC.title_is("webdriver - Поиск в Google"))
