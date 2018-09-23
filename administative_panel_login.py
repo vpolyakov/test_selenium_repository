ca03bd04c5670022896199f25a4f1e3bd16166b5
Vladimir Polyakov <vlpolyakov@mail.ru>
2018-09-24 00:41:48 +0400

    Задание 3. Добавил сценарий логина в панель управления интернет магазина

---
+++ administative_panel_login.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://localhost/litecart/admin/")
+    wait = WebDriverWait(driver, 10)  # seconds
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("login").click()
+    wait.until(EC.visibility_of_element_located((By.ID, "platform")))
+
