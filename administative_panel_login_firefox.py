0f8dac97f9373ef6b74876ee506940e4505ce9c6
Vladimir Polyakov <vlpolyakov@mail.ru>
2018-09-25 00:06:43 +0400

    Задание 4. Добавил запуск в Ferifox и Edge

---
+++ administative_panel_login_firefox.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Firefox()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://localhost/litecart/admin/")
+    wait = WebDriverWait(driver, 10)  # seconds
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("login").click()
+    wait.until(EC.visibility_of_element_located((By.ID, "platform")))
