49ded633603fa10e51b429197a7f2cdbe26f2e93
Vladimir Polyakov <vlpolyakov@mail.ru>
2018-10-24 04:14:41 +0400

    Убирание неиспользуемых строк программ

--- task 17.py
+++ task 17.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import time
 
 
 driver = webdriver.Chrome()
